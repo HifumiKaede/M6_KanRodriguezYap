06fd67784c2113234ee6bddb6a6ee7af758ccd91
felipekantcode <antonio.felipe.kan@student.ateneo.edu>
2026-04-11 00:45:23 +0800

    Update urls.py

--- M6E_KanRodriguezYap/tapas/tapasapp/urls.py
+++ M6E_KanRodriguezYap/tapas/tapasapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ACCOUNT
     path('', views.login_view, name='login'),
     path('signup/', views.signup_view, name='signup'),
     path('basic_list/<int:pk>/', views.basic_list, name='basic_list'),
@@ -9,4 +10,11 @@ urlpatterns = [
     path('change_password/<int:pk>/', views.change_password, name='change_password'),
     path('delete_account/<int:pk>/', views.delete_account, name='delete_account'),
     path('logout/', views.logout_view, name='logout'),
+
+    # DISH
+    path('menu/', views.better_menu, name='better_menu'),
+    path('menu/add/', views.add_menu, name='add_menu'),
+    path('menu/view/<int:pk>/', views.view_detail, name='view_detail'),
+    path('menu/update/<int:pk>/', views.update_dish, name='update_dish'),
+    path('menu/delete/<int:pk>/', views.delete_dish, name='delete_dish'),
 ]
